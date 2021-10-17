c3b3064e428c7448179328144b814ca7f0228f9c
soheil saedi fard <soheil.saedi@ymail.com>
2021-10-17 22:23:50 +0330

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,91 @@
+const printNumbers = (input) => {
+    if (!input && input !== 0) {
+        return ''
+    }
+    input = input.toString()
+    let arrInput = input.split('');
+    let Result = ''
+    arrInput.map(num => {
+        Result += `${num}: `
+        for (let i = 0; i < num; i++) {
+            Result += num;
+        }
+        Result += '\n'
+    })
+    return Result;
+}
+
+console.log(printNumbers(123))
+
+function isSubstring(string, substring) {
+    for (let i = 0; i < string.length; i++) {
+        const sliced = string.slice(i, i + substring.length);
+        if (sliced === substring) {
+            console.log(substring.length)
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(isSubstring('hello world', 'world'));
+console.log(isSubstring('hello world', 'World'));
+
+function printSign(n, sign = '*') {
+    let res = ''
+    for (let i = 0; i < n; i++) {
+        if (i === 0 || i === n - 1) {
+            res += sign.repeat(n) + '\n'
+        } else {
+            res += sign + ' '.repeat(n-2) +sign + '\n'
+        }
+    }
+    return res
+}
+
+console.log(printSign(5))
+
+function Submission(arr1, arr2) {
+    return arr1.filter(item => !arr2.includes(item))
+}
+
+console.log(Submission([1,2,3],[1]))
+
+function expl1(arr, index, offset) {
+    if (0 > index + offset || arr.length < offset + index +1) {
+        return console.error("Invalid array")
+    } const arr2 = arr.splice(index,1)[0]
+    arr.splice(index+offset,0,arr2)
+    return arr
+}
+
+console.log(expl1([1, 2, 3, 4], 2, 0))
+
+function reverse(arr) {
+    const newArr = arr.split("")
+    console.log(newArr)
+    let arr2 = []
+    for (let i = 0; arr.length > i + 1; i++) {
+        arr2 = newArr.pop();
+    } return arr2
+}
+
+reverse("abcd")
+
+function rec(str){
+    return str ? rec(str.substr(1)) + str[0] : str
+}
+
+console.log(rec("abcd"))
+
+function sortStr(str) {
+    let result = ''
+    for (let i = 0; str.length > i; i++) {
+        if (str.substring(i, i + 1) !== str.substring(i + 1, i + 2)) {
+            result += str.substring(i, i + 1)
+        }
+    }  return result
+
+}
+
+console.log(sortStr('aabbfvv'))
